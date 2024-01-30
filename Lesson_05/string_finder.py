9ffd0d888af4adb6235991da329421af1f62ab1e
Giorgio <puncho_giorgio@inbox.lv>
2024-01-30 10:00:41 +0200

    HomeWork_03

--- Lesson_05/string_finder.py
+++ Lesson_05/string_finder.py
@@ -12,10 +12,9 @@ for i in mylist:
     if text.lower() == i[2:-3].lower():
         test_list.append(i[2:-3])
         test_list.append(i)
-        break
 
 if len(test_list):
     print(f"The searched word '{test_list[0]}' has found, at index {mylist.index(test_list[1])}")
-    
+
 elif not len(test_list):
     print("There is no such word in the list")
